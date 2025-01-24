f2fd9a53308a1e6bfad3476e3a5312943a65a8b2
Nikhil27081997 <nikhilsapkale025@gmail.comgit config --global user.name>
2025-01-24 21:00:18 +0530

    used lambda function

--- Example5.py
+++ Example5.py
@@ -3,5 +3,15 @@ for i in lis:
     print(i*i*i)
 
 lis=[1,2,3,4,5,6,7,8,9,10]
-s=list(filter(lambda x: x%2==1,lis))
+s=list(filter(lambda x: x%2==0,lis))
 print(s)
+
+lis=[10,20,30,40,50]
+x=list(map(lambda x:x*2,lis))
+print(x)
+
+
+from functools import reduce #pkg/library
+lis=[10,20,30,40,50]
+x=reduce (lambda x,y:x+y, lis)
+print(x)
